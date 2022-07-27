f1eafa6f5c943e1a3f9d1fa91c9be500192fe9d5
ivan-adi-saputra <ivanadisaputra34@gmail.com>
2022-07-27 07:49:29 +0700

    Convensi HTML slicing to Laravel Blade

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,15 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardProductController;
+use App\Http\Controllers\DashboardSettingController;
+use App\Http\Controllers\DashboardTransactionController;
+use App\Http\Controllers\DetailController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +23,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('categories', [CategoryController::class, 'index'])->name('categories');
+
+Route::get('details/{id}', [DetailController::class, 'index'])->name('details');
+
+Route::get('cart', [CartController::class, 'index'])->name('cart');
+Route::get('success', [CartController::class, 'success'])->name('success');
+
+Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('dashboard/products', [DashboardProductController::class, 'index'])->name('dashboard-product');
+Route::get('dashboard/products/create', [DashboardProductController::class, 'create'])->name('dashboard-product-create');
+Route::get('dashboard/products/{id}', [DashboardProductController::class, 'details'])->name('dashboard-product-details');
+
+Route::get('dashboard/transaction', [DashboardTransactionController::class, 'index'])->name('dashboard-transaction');
+Route::get('dashboard/transaction/{id}', [DashboardTransactionController::class, 'details'])->name('dashboard-transaction-details');
+
+Route::get('dashboard/setting', [DashboardSettingController::class, 'setting'])->name('dashboard-settings');
+Route::get('dashboard/account', [DashboardSettingController::class, 'account'])->name('dashboard-account');
+
+Route::get('register', [RegisterController::class, 'index'])->name('register-success');
+Route::get('register/success', [RegisterController::class, 'success'])->name('register-success');
+
+Route::get('login', [LoginController::class, 'index'])->name('login');
